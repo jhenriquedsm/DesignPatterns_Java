f094637dd8d366644fd008b5e94f898bda9be481
jhenriquedsm <jhsilvamata@gmail.com>
2023-08-07 14:38:51 -0300

    Padrão de Projeto - Strategy

--- loja/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
+++ loja/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
@@ -4,16 +4,17 @@ import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public class CalculadoraDeImpostos {
+/*
+PADRÃO DE PROJETO - STRATEGY
+    Cria-se uma estratégia a fim de evitar um código cheio de "if e else" que cresce cada vez mais de acordo com a necessidade de
+    novas implementações.
+    Deixa o código mais coeso, fazendo a calculadora estar vinculada somente a interface.
+        - deixa o código menor e menos dependente;
+    O Strategy visa resolver o problema de diversos algoritmos para uma ação que resulta na possibilidade de vários ifs.
+*/
 
-    public BigDecimal calcular (Orcamento orcamento, TipoImposto tipoImposto){
-        switch (tipoImposto) {
-            case ICMS:
-                return orcamento.getValor().multiply(new BigDecimal("0.1"));
-            case ISS:
-                return orcamento.getValor().multiply(new BigDecimal("0.06"));
-            default:
-                return BigDecimal.ZERO;
-        }
+public class CalculadoraDeImpostos {
+    public BigDecimal calcular (Orcamento orcamento, Imposto imposto) {
+        return imposto.calcular(orcamento);
     }
 }
