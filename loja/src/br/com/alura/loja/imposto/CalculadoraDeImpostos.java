9072cc4bc0035cadd55a74c8216a114927dce7a5
jhenriquedsm <jhsilvamata@gmail.com>
2023-08-07 14:23:13 -0300

    implementação dos impostos sem a utilização de padrão de projeto

--- loja/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
+++ loja/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
@@ -6,8 +6,14 @@ import java.math.BigDecimal;
 
 public class CalculadoraDeImpostos {
 
-    public BigDecimal calcular (Orcamento orcamento){
-        return orcamento.getValor().multiply(new BigDecimal("0.1"));
+    public BigDecimal calcular (Orcamento orcamento, TipoImposto tipoImposto){
+        switch (tipoImposto) {
+            case ICMS:
+                return orcamento.getValor().multiply(new BigDecimal("0.1"));
+            case ISS:
+                return orcamento.getValor().multiply(new BigDecimal("0.06"));
+            default:
+                return BigDecimal.ZERO;
+        }
     }
-
 }
