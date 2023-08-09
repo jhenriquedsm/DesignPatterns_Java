aae102cb7bcd8e5976f47b098317400251bc2681
jhenriquedsm <jhsilvamata@gmail.com>
2023-08-09 16:00:22 -0300

    implementação do metódo de desconto extra sem o uso de padrões de projeto

--- loja/src/br/com/alura/loja/orcamento/Orcamento.java
+++ loja/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -6,10 +6,26 @@ public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeItens;
+    private String situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+    }
+
+    public void aplicarDescontoExtra(){
+        BigDecimal valorDescontoExtra = BigDecimal.ZERO;
+        if(situacao.equals("EM ANALISE")){
+            valorDescontoExtra = new BigDecimal("0.05");
+        } else if (situacao.equals("APROVADO")) {
+            valorDescontoExtra = new BigDecimal("0.02");
+        }
+
+        this.valor = this.valor.subtract(valorDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao = "APROVADO";
     }
 
     public BigDecimal getValor() {
